f1730765cae2b4cef404a8b1ba08645e302f0f85
NoEndToLF <wangxiaoyun@zhufaner.com>
2017-11-24 18:30:28 +0800

    first finish

--- app/src/main/java/com/wstatic/toolandstatusbar/StatusColorActivity.java
+++ app/src/main/java/com/wstatic/toolandstatusbar/StatusColorActivity.java
@@ -4,16 +4,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 import com.wstatic.toolandstatusbarlibrary.ToolAndStatusBarHelp;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class StatusColorActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.btn_status_textcolor_black)
+    Button btnStatusTextcolorBlack;
+    @BindView(R.id.btn_status_textcolor_white)
+    Button btnStatusTextcolorWhite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +27,9 @@ public class StatusColorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_status_color);
         ButterKnife.bind(this);
         setToolBar();
-        ToolAndStatusBarHelp.getInstance().setStatusBarColor(this,R.color.color_statusbar);
+        ToolAndStatusBarHelp.getInstance().setStatusBarColor(this, R.color.color_statusbar);
     }
+
     private void setToolBar() {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -36,5 +43,17 @@ public class StatusColorActivity extends AppCompatActivity {
             }
         });
         toolbar.setBackgroundColor(getResources().getColor(R.color.color_toolbar));
+    }
+
+    @OnClick({R.id.btn_status_textcolor_black, R.id.btn_status_textcolor_white})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_status_textcolor_black:
+                ToolAndStatusBarHelp.getInstance().setStatusBarColor(this, R.color.color_statusbar,true);
+                break;
+            case R.id.btn_status_textcolor_white:
+                ToolAndStatusBarHelp.getInstance().setStatusBarColor(this, R.color.color_statusbar,false);
+                break;
+        }
     }
 }
