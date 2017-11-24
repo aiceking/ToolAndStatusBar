f5e992cfa5e2f3ee0e910928da47b4b2db2bf5d5
NoEndToLF <wangxiaoyun@zhufaner.com>
2017-11-24 18:06:02 +0800

    sample_four

--- app/src/main/java/com/wstatic/toolandstatusbar/StatusTransparentActivity.java
+++ app/src/main/java/com/wstatic/toolandstatusbar/StatusTransparentActivity.java
@@ -3,11 +3,14 @@ package com.wstatic.toolandstatusbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.wstatic.toolandstatusbarlibrary.ToolAndStatusBarHelp;
+
 public class StatusTransparentActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status_transparent);
+        ToolAndStatusBarHelp.getInstance().setStatusBarTransparent(this);
     }
 }
