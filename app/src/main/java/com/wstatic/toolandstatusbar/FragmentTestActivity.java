bbc5c556feed634eab32e22a464db7f11b059df3
NoEndToLF <wangxiaoyun@zhufaner.com>
2017-11-25 16:34:31 +0800

    优化

--- app/src/main/java/com/wstatic/toolandstatusbar/FragmentTestActivity.java
+++ app/src/main/java/com/wstatic/toolandstatusbar/FragmentTestActivity.java
@@ -1,13 +1,84 @@
 package com.wstatic.toolandstatusbar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+
+import com.wstatic.toolandstatusbar.fragment.CollFragment;
+import com.wstatic.toolandstatusbar.fragment.CommonFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class FragmentTestActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.tablayout)
+    TabLayout tablayout;
+    @BindView(R.id.linear_parent)
+    LinearLayout linearParent;
+    private List<String> list_titles;
+    private List<Fragment> list_fragments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_test);
+        ButterKnife.bind(this);
+        initTitleAndFragment();
+        tablayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                CommonFragment fragment = new CommonFragment();
+                Bundle bundle = new Bundle();
+                bundle.putString("title", list_titles.get(tab.getPosition()));
+                bundle.putInt("position", tab.getPosition());
+                fragment.setArguments(bundle);
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction transaction = fm.beginTransaction();
+                transaction.replace(R.id.linear_parent, fragment);
+                transaction.commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
+
+    private void initTitleAndFragment() {
+        list_titles = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            list_titles.add("测试 " + (i + 1));
+        }
+        list_fragments = new ArrayList<>();
+        for (int i = 0; i < list_titles.size(); i++) {
+            tablayout.addTab(tablayout.newTab().setText(list_titles.get(i)));
+            CommonFragment fragment = new CommonFragment();
+            Bundle bundle = new Bundle();
+            bundle.putString("title", list_titles.get(i));
+            bundle.putInt("position", i);
+            fragment.setArguments(bundle);
+            list_fragments.add(fragment);
+        }
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.linear_parent, list_fragments.get(0));
+        transaction.commit();
+    }
+
+
 }
---
+++ app/src/main/java/com/wstatic/toolandstatusbar/fragment/CommonFragment.java
@@ -0,0 +1,66 @@
+package com.wstatic.toolandstatusbar.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.wstatic.toolandstatusbar.R;
+import com.wstatic.toolandstatusbarlibrary.ToolAndStatusBarHelp;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * Created by static on 2017/11/25/025.
+ */
+
+public class CommonFragment extends Fragment {
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    private View rootView;
+    private String title;
+    private int position;
+    private int []Colors={R.color.color_one,
+            R.color.color_two,
+            R.color.color_three,
+            R.color.color_four,};
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+            rootView = inflater.inflate(R.layout.activity_tool_status, container, false);
+            ButterKnife.bind(this, rootView);
+            title=getArguments().getString("title");
+            position=getArguments().getInt("position");
+            ToolAndStatusBarHelp.getInstance().setToolAndStatusBarColor(this,rootView, Colors[position]);
+
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setToolBar();
+    }
+
+    private void setToolBar() {
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayShowTitleEnabled(true);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ((AppCompatActivity) getActivity()).setTitle(title);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().finish();
+            }
+        });
+    }
+
+
+}
