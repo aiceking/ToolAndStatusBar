baf88b27a3712b077bf3171fe9da1757907f2520
NoEndToLF <wangxiaoyun@zhufaner.com>
2017-11-27 14:20:22 +0800

    优化

--- app/src/main/java/com/wstatic/toolandstatusbar/DrawerActivity.java
+++ app/src/main/java/com/wstatic/toolandstatusbar/DrawerActivity.java
@@ -1,12 +1,16 @@
 package com.wstatic.toolandstatusbar;
 
+import android.content.Context;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.wstatic.toolandstatusbarlibrary.ToolAndStatusBarHelp;
 
@@ -30,6 +34,12 @@ public class DrawerActivity extends AppCompatActivity {
         setToolBar();
     }
     private void setToolBar() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            LinearLayout.LayoutParams lp=new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT);
+            lp.setMargins(0,dip2px(this,25),0,0);
+            toolbar.setLayoutParams(lp);
+        }
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -44,7 +54,10 @@ public class DrawerActivity extends AppCompatActivity {
         });
         toolbar.setBackgroundColor(getResources().getColor(R.color.color_toolbar));
     }
-
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
